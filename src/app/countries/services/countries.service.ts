7c27728894b41e2ae02a4d6cd9fc1819b3d86182
AlbertoG22 <albertogarciamartinez99@gmail.com>
2024-05-13 17:30:01 -0600

    search by country observable

--- src/app/countries/services/countries.service.ts
+++ src/app/countries/services/countries.service.ts
@@ -11,8 +11,16 @@ export class CountriesService {
   constructor(private http: HttpClient) { }
 
   searchCapital( term: string ): Observable<Country[]> {
-
     const url = `${ this.apiUrl }/capital/${ term }`;
+
+    return this.http.get<Country[]>( url )
+      .pipe(
+        catchError( (error) => of([]) )
+      );
+  }
+
+  searchCountry( term: string ): Observable<Country[]> {
+    const url = `${ this.apiUrl }/name/${ term }`;
 
     return this.http.get<Country[]>( url )
       .pipe(
