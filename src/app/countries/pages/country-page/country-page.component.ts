3f3104a24f7de117873f435892d79c01b5144d2b
AlbertoG22 <albertogarciamartinez99@gmail.com>
2024-05-14 11:06:13 -0600

    making request to the api with alpha code

--- src/app/countries/pages/country-page/country-page.component.ts
+++ src/app/countries/pages/country-page/country-page.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { CountriesService } from '../../services/countries.service';
 
 @Component({
   selector: 'app-country-page',
@@ -8,14 +9,20 @@ import { ActivatedRoute } from '@angular/router';
   ]
 })
 export class CountryPageComponent implements OnInit {
-  constructor( private activatedRoute: ActivatedRoute ) {}
+
+  constructor(
+    private activatedRoute: ActivatedRoute,
+    private countriesService: CountriesService
+  ) {}
 
   ngOnInit(): void {
-   this.activatedRoute.params
+    this.activatedRoute.params
     .subscribe( ({ id }) => {
 
-      console.log({ params: id });
-
+      this.countriesService.searchCountryByAlphaCode( id )
+        .subscribe( country => {
+          console.log({ country });
+        });
     });
   }
 }
