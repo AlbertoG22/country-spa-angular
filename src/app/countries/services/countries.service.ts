a0a1ec61faf599cb7b1b17eb93fdfba36236731e
AlbertoG22 <albertogarciamartinez99@gmail.com>
2024-05-16 17:58:25 -0600

    maintain data between screens, component: byCapitalPage

--- src/app/countries/services/countries.service.ts
+++ src/app/countries/services/countries.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Observable, catchError, delay, map, of } from 'rxjs';
+import { Observable, catchError, delay, map, of, tap } from 'rxjs';
 import { Country } from '../interfaces/country';
 import { CacheStore } from '../interfaces/cache-store.interface';
 
@@ -21,8 +21,7 @@ export class CountriesService {
 
     return this.http.get<Country[]>( url )
       .pipe(
-        catchError( (error) => of([]) ),
-        // delay(2000)
+        catchError( () => of([]) ),
       );
   }
 
@@ -39,7 +38,10 @@ export class CountriesService {
   searchCapital( term: string ): Observable<Country[]> {
     const url = `${ this.apiUrl }/capital/${ term }`;
 
-    return this.getCountriesRequest( url );
+    return this.getCountriesRequest( url )
+      .pipe(
+        tap( countries => this.cacheStore.byCapital = { term, countries })
+      );
   }
 
   searchCountry( term: string ): Observable<Country[]> {
